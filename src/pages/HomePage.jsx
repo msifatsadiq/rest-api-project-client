1e94765c1f8c5db7f18669da335284a8f86c6131
msifatsadiq <msifatsadiq@gmail.com>
2025-07-23 15:51:23 +0600

    adding note not found page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { toast } from "react-hot-toast"
 import Navbar from "../components/Navbar"
 import NoteCard from "../components/NoteCard"
+import NotesNotFound from "../components/NotesNotFound"
 import RateLimitedUi from "../components/RateLimitedUi"
 import api from "../lib/axios"
 
@@ -38,6 +39,7 @@ const HomePage = () => {
             {IsRateLimited && <RateLimitedUi />}
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 {loading && <div className="text-center text-lg py-10 ">Loading...</div>}
+                {notes.length === 0 && !IsRateLimited && <NotesNotFound />}
 
                 {notes.length > 0 && !IsRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
